ae5160458eb5a3456633c74a587841470c7a16b0
MYCOMPUTER\minto <mintopazu@gmail.com>
2017-10-12 06:47:34 +0900

    first commit 球打ちだしまで

---
+++ Assets/script/ball_add_force.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_add_force : MonoBehaviour {
+
+    public Rigidbody rb;        //重力
+    public float coefficient;   // 空気抵抗係数
+
+    void Start () {
+
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(-10.0f, 0.0f, 15.0f * Random.value + 10.0f);
+        Debug.Log(rb.velocity);
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            rb.velocity = new Vector3( -10.0f, 0.0f, 15.0f * Random.value + 10.0f);
+            Debug.Log(rb.velocity);
+            this.transform.position = new Vector3(11.45f, 12.60f, 0.0f);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        // 空気抵抗を与える
+        rb.AddForce(-coefficient * rb.velocity);
+    }
+}
---
+++ Assets/script/rotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotation : MonoBehaviour {
+    public Transform tr;
+    System.Diagnostics.Stopwatch sw;
+
+
+    void Start () {
+        
+    }
+	
+	
+	void Update () {
+        tr.Rotate(0, 2f, 0);
+       
+    }
+}
