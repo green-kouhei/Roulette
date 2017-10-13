8b7df6226e67e281d5327762efe3f6d1a36d6d33
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-10-13 18:48:02 +0900

    範囲外エラー対策

--- Assets/script/NumberText.cs
+++ Assets/script/NumberText.cs
@@ -5,16 +5,18 @@ using UnityEngine.UI;
 
 public class NumberText : MonoBehaviour {
     int[] NUMBERS = { -1, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2, 0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1 };
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         int index = getSpotIndex();
+        if (index >= 0 && index < 38) { 
         Text text = GetComponent<Text>();
         text.text = NUMBERS[index].ToString();
+        }
     }
     int getSpotIndex()
     {
