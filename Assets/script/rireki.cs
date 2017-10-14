777eff01cfc0e19ef7729a4b7d3800ef94d77665
kuma_note <Hentai>
2017-10-14 15:13:03 +0900

    10/14 履歴を表示する場所だけつくったのん

---
+++ Assets/script/rireki.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rireki : MonoBehaviour {
+
+GUIStyle guiStyle = new GUIStyle();
+GUIStyleState styleState = new GUIStyleState();
+
+void OnGUI () {
+
+styleState.textColor = Color.green;
+guiStyle.normal = styleState;
+
+// ラベルを表示する
+GUI.Label(new Rect(200, 200, 100, 50), "rireki", guiStyle);
+}
+
+}
