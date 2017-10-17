0b8a1a6b0dd9d5d548d9d30c1c9cb619487b20c7
kuma_note <Hentai>
2017-10-17 14:09:20 +0900

    10/17 履歴表示(色と00の処理まだ)

--- Assets/script/NumberText.cs
+++ Assets/script/NumberText.cs
@@ -8,10 +8,15 @@ public class NumberText : MonoBehaviour {
     private int mLastIndex = -2;
     private bool mFlag = false;
     private string mNum="";
-
+    Rireki re;
     int[] NUMBERS = { -1, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2, 0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1 };
+
     // Use this for initialization
     void Start() {
+         
+
+        re = GameObject.Find("CanvasR").GetComponent<Rireki>();
+
 
     }
 
@@ -35,6 +40,7 @@ public class NumberText : MonoBehaviour {
                 {
                     mNum = NUMBERS[index].ToString();
                     mFlag = false;
+                    re.printRireki(mNum);
                 }
             }
         }
---
+++ Assets/script/Rireki.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Rireki : MonoBehaviour {
+    public Text[] re;
+    string[] st = { "", "", "", "", "", "", "", "", "" };
+
+
+
+    public void printRireki(string num)
+    {
+        //re[0].text = num;
+        for (int i = st.Length-1;i>0;i--)
+        {
+            st[i] = st[i - 1];
+        }
+        st[0] = num;
+        for(int i = 0; i < re.Length; i++)
+        {
+            re[i].text = st[i];
+        }
+
+    }
+}
