27246ef1cda42d2dade0ad4b8ae61be90027d8bc
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-10-18 16:51:55 +0900

    スコア表示サンプルの追加

---
+++ Assets/GUITest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GUITest : MonoBehaviour {
+    public GUIStyle headerStyle;
+    public GUIStyle numberStyle;
+    public int[] numbers = new int[9];
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnGUI()
+    {
+        int y = Screen.height-33*2;
+
+        int width = 64;
+        int height = 32;
+        for(int i=0;i<9; i++)
+        {
+            GUI.Box(new Rect((width + 1) * i,y, width, height), (i + 1).ToString(), headerStyle);
+            numberStyle.normal.textColor = new Color(1.0f, 0.0f, 0.0f);
+            GUI.Box(new Rect((width + 1) * i,y+ height+1, width, height), numbers[i].ToString(), numberStyle);
+        }
+        
+    }
+}
--- Assets/script/RouletteOperation.cs
+++ Assets/script/RouletteOperation.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RouletteOperation : MonoBehaviour {
-    public float speed = 2000.0f;
+    public float speed = 1500.0f;
     public GameObject rotationBase;
 	public GameObject ball;
 
