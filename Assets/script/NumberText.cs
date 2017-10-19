870fa6731bdec19f35c254bf39650453f23af83f
kuma_note <Hentai>
2017-10-19 15:13:35 +0900

    10/19 画面依存しないのだ

--- Assets/script/NumberText.cs
+++ Assets/script/NumberText.cs
@@ -9,7 +9,7 @@ public class NumberText : MonoBehaviour {
     private int mLastIndex = -2;
     private bool mFlag = false;
     private string mNum="";
-    Rireki re;
+    GUITest re;
     int[] NUMBERS = { -1, 27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2, 0, 28, 9, 26, 30, 11, 7, 20, 32, 17, 5, 22, 34, 15, 3, 24, 36, 13, 1 };
 
 
@@ -17,7 +17,7 @@ public class NumberText : MonoBehaviour {
     void Start() {
          
 
-        re = GameObject.Find("CanvasR").GetComponent<Rireki>();
+        re = GameObject.Find("History").GetComponent<GUITest>();
 
 
     }
